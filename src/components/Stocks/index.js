2e32d4cc2d580423646783c208f304419030c1ab
Scott Shaffer <scottwshaffer1@gmail.com>
2022-09-03 18:17:25 -0600

    practiced NavLinks and related hooks

--- src/components/Stocks/index.js
+++ src/components/Stocks/index.js
@@ -1,6 +1,21 @@
+import { Redirect, useHistory } from "react-router-dom";
+
 function Stocks() {
+  let loggedIn = true;
+  let history = useHistory();
+
+  const handleClick = () => {
+    window.alert("Sending info to the DB!");
+    history.push("/");
+  };
+
+  if (!loggedIn) {
+    return <Redirect to="/not-logged-in" />;
+  }
+
   return (
-    <div className='comp orange'>
+    <div className="comp orange">
+      <button onClick={handleClick}>Home</button>
       <h1>Stocks Component</h1>
     </div>
   );
